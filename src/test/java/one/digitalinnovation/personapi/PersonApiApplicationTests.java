5161b3b9f233a5984cd9388d55a040275d7e8a1e
Atanes <aatanes100@gmail.com>
2022-02-04 19:36:52 -0300

    Inclusão de classes de test

--- src/test/java/one/digitalinnovation/personapi/PersonApiApplicationTests.java
+++ src/test/java/one/digitalinnovation/personapi/PersonApiApplicationTests.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PersonApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+class PersonapiApplicationTests {
+    @Test
+    void contextLoads() {
+    }
 }
